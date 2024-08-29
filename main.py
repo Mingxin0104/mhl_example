c097cf6f99e94aa4f8331f69f1439e4ad65b2215
Mingxin0104 <155360787+Mingxin0104@users.noreply.github.com>
2024-08-29 12:10:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,20 +26,19 @@ seed_torch(4)
 def make_train_valid_dfs():
     # dataframe = pd.read_csv(args.node_entity_matching_path, sep='\t')
     # max_id = dataframe.shape[0] if not args.debug else args.number_samples
-    # text_ids = np.arange(0, max_id)
+    # mm_ids = np.arange(0, max_id)
     # np.random.seed(42)
     # valid_ids = np.random.choice(
-    #     text_ids, size=int(0.2 * len(text_ids)), replace=False
+    #     mm_ids, size=int(0.2 * len(mm_ids)), replace=False
     # )
 
-    # train_ids = [id_ for id_ in text_ids if id_ not in valid_ids]
+    # train_ids = [id_ for id_ in mm_ids if id_ not in valid_ids]
     # dataframe['id'] = list(dataframe.index)
     # train_dataframe = dataframe[dataframe["id"].isin(train_ids)].reset_index(drop=True)
     # valid_dataframe = dataframe[dataframe["id"].isin(valid_ids)].reset_index(drop=True)
     # train_dataframe.to_csv('id_labels.csv')
 
     dataframe = pd.read_csv('id_labels.csv')
-    # dataframe["text"] = "hi"
     dataframe["entity_id"] = dataframe["id"]
     train_dataframe = dataframe[:(3624-717)]
     valid_dataframe = dataframe[(3624-717):]
@@ -47,17 +46,10 @@ def make_train_valid_dfs():
     return train_dataframe, valid_dataframe
 
 def build_loaders(dataframe, mode):
-    # dataset = NodeTextDataset(
-    #     dataframe["entity_id"].values,
-    #     dataframe["text"].values,
-    #     dataframe['label'].values,
-    #     tokenizer=tokenizer
-    # )
-    dataset = NodeTextDataset(
+
+    dataset = NodeMMDataset(
         dataframe["entity_id"].values,
-        # dataframe["text"].values,
-        label=dataframe['label'].values,
-        # tokenizer=tokenizer
+        label=dataframe['label'].values
     )
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -72,18 +64,18 @@ def train_epoch(model, feature,adj,train_loader, optimizer, lr_scheduler, step,p
     tqdm_object = tqdm(train_loader, total=len(train_loader))
     print(train_loader)
     for batch in tqdm_object:
-        batch = {k: v.to(args.device) for k, v in batch.items() if k != "text"}
+        batch = {k: v.to(args.device) for k, v in batch.items() if k != "mm"}
         # print(batch)
         loss, node_embed_prune,node_embeds = model(batch, feature, adj, pos)
         optimizer.zero_grad()
         if args.prune:
             for name, module in model.named_modules():
-                if 'text_encoder.model' in name:
-                    if args.text_encoder_model == '':
+                if 'mm_encoder.model' in name:
+                    if args.mm_encoder_model == '':
                         if '.attention.self.q_lin' in name or '.attention.self.k_lin' in name or '.attention.self.v_lin' in name or '.attention.out_lin' in name or '.ffn.lin' in name:
                             module.weight = module.weight_orig.clone()
                             module.bias = module.bias_orig.clone()
-                    elif args.text_encoder_model == '':
+                    elif args.mm_encoder_model == '':
                         if '.attention.self.query' in name or '.attention.self.key' in name or '.attention.self.value' in name or '.attention.output.dense' in name or '.intermediate.dense' in name or '.intermediate.dense' in name or '.attention.output.dense' in name or 'model.pooler.dense' in name:
                             module.weight = module.weight_orig.clone()
                             module.bias = module.bias_orig.clone()
@@ -109,7 +101,7 @@ def valid_epoch(model, feature,adj,valid_loader,pos):
     loss_meter = AvgMeter()
     tqdm_object = tqdm(valid_loader, total=len(valid_loader))
     for batch in tqdm_object:
-        batch = {k: v.to(args.device) for k, v in batch.items() if k != "text"}
+        batch = {k: v.to(args.device) for k, v in batch.items() if k != "mm"}
         loss, node_embed_prune,node_embeds = model(batch, feature, adj,pos)
 
         count = batch["entity"].size(0)
@@ -123,10 +115,10 @@ def main():
     my_time = time.strftime('%Y%m%d%H%M', time.gmtime(time.time()))
 
     train_df, valid_df = make_train_valid_dfs()   # make train, validation datasets
-    if args.text_encoder_model == '':
-        tokenizer = DistilBertTokenizer.from_pretrained(args.text_encoder_tokenizer) # read distil-bert language model
-    elif args.text_encoder_model == '':
-        tokenizer = BertTokenizer.from_pretrained(args.text_encoder_tokenizer)  # read bert language model
+    if args.mm_encoder_model == '':
+        tokenizer = DistilBertTokenizer.from_pretrained(args.mm_encoder_tokenizer) # read distil-bert language model
+    elif args.mm_encoder_model == '':
+        tokenizer = BertTokenizer.from_pretrained(args.mm_encoder_tokenizer)  # read bert language model
     train_loader = build_loaders(train_df, mode="train")     #
     valid_loader = build_loaders(valid_df ,mode="valid")
 
@@ -135,12 +127,12 @@ def main():
 
     if args.prune:
         for name, module in model.named_modules():
-            if 'text_encoder.model' in name:
-                if args.text_encoder_model == '':
+            if 'mm_encoder.model' in name:
+                if args.mm_encoder_model == '':
                     if '.attention.self.q_lin' in name or '.attention.self.k_lin' in name or '.attention.self.v_lin' in name or '.attention.out_lin' in name or '.ffn.lin' in name:
                         prune.l1_unstructured(module, name='weight', amount=int(module.weight.shape[0]*module.weight.shape[1]*args.prune_ratio))
                         prune.l1_unstructured(module, name='bias', amount=int(module.bias.shape[0]  * args.prune_ratio))
-                elif args.text_encoder_model == '':
+                elif args.mm_encoder_model == '':
                     if '.attention.self.query' in name or '.attention.self.key' in name or '.attention.self.value' in name or '.attention.output.dense' in name or '.intermediate.dense' in name or '.intermediate.dense' in name or '.attention.output.dense' in name or 'model.pooler.dense' in name:
                         prune.l1_unstructured(module, name='weight', amount=int(module.weight.shape[0] * module.weight.shape[1] * args.prune_ratio))
                         prune.l1_unstructured(module, name='bias', amount=int(module.bias.shape[0] * args.prune_ratio))
@@ -166,7 +158,7 @@ def main():
         train_loss,node_embed_prune_train,node_embeds_train = train_epoch(model,features,adj, train_loader, optimizer, lr_scheduler, step,pos)
         model.eval()
 
-        save_model_path = args.finetune_node_model_path #"./pretrain/{}_node_text_{}.pt".format(args.dataset, my_time)
+        save_model_path = args.finetune_node_model_path #"./pretrain/{}_node_mm_{}.pt".format(args.dataset, my_time)
 
         if epoch % 1 == 0:
             with torch.no_grad():
@@ -184,8 +176,8 @@ def main():
         finetune_feature_path = args.finetune_feature_path #r'./finetune/{}_data/finetune_feature_{}.txt'.format(args.dataset,my_time)
         fo = open(finetune_feature_path, 'w', encoding='utf8')
         for i in range(df.shape[0]):
-            # embed_ = model.text_encoder.get_finetune_embed(df.loc[i, 'content'])
-            embed_ = model.text_encoder.forward(features['feature'])
+            # embed_ = model.mm_encoder.get_finetune_embed(df.loc[i, 'content'])
+            embed_ = model.mm_encoder.forward(features['feature'])
             # np.savetxt(fo, np.array(embed_.detach().cpu()).reshape([1, 768]))
             if i % 1000 == 0:
                 print("{} fine-tuned features have been generated!".format(i))
